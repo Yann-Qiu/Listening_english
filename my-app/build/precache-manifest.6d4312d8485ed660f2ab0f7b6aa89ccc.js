894135c0dee589bcdfc337c5ce9e1355db2c0ec6
qs4091 <56961747+qs4091@users.noreply.github.com>
2020-03-18 00:46:54 +0100

    after run build
    after run build

---
+++ my-app/build/precache-manifest.6d4312d8485ed660f2ab0f7b6aa89ccc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "773575b57af61495ab6a4a696fc148f0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "807c4137b9818956de72",
+    "url": "./static/css/2.9ddb28ea.chunk.css"
+  },
+  {
+    "revision": "9ac774aa84343375f08c",
+    "url": "./static/css/main.4f89796e.chunk.css"
+  },
+  {
+    "revision": "807c4137b9818956de72",
+    "url": "./static/js/2.eb0ccdec.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "./static/js/2.eb0ccdec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ac774aa84343375f08c",
+    "url": "./static/js/main.efdea3ef.chunk.js"
+  },
+  {
+    "revision": "567ca5cea940a0c54188",
+    "url": "./static/js/runtime-main.1a632c21.js"
+  }
+]);
